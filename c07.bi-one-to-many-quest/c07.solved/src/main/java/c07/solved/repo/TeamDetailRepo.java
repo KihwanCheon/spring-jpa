408d23aa9cfdbd868c0d96e03cbb095dd0631203
KihwanCheon <gujjy97@gmail.com>
2022-03-05 16:52:32 +0900

    Delete useless repo interface

--- c07.bi-one-to-many-quest/c07.solved/src/main/java/c07/solved/repo/TeamDetailRepo.java
+++
@@ -1,7 +0,0 @@
-package c07.solved.repo;
-
-import c07.solved.entity.TeamDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamDetailRepo extends JpaRepository<TeamDetail, Long> {
-}
